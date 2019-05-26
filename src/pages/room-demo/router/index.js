77692aa5cf476fec29beb758ca266f1e5d2ed766
zhanglei <zhanglei@everjiankang.com>
2019-05-26 13:59:49 +0800

    改造demo工作站

---
+++ src/pages/room-demo/router/index.js
@@ -0,0 +1,131 @@
+import Resource from '../resource'
+import Axios from '../axios'
+import Route from '../route'
+import RouteDtail from '../routeDtail'
+import RouteDtail2 from '../routeDtail2'
+import RouteRes from '../routeRes'
+import RoutePush from '../routePush'
+import RouteSon from '../routeSon'
+import RouteSon1 from '../routeSon1'
+import RouteSon2 from '../routeSon2'
+import MintUI from '../mintUI'
+import ElementUI from '../elementUI'
+import Es6 from '../es6'
+import DemoHome from '../home'
+import Aaa from '../aaa'
+import Style from '../style'
+import Eventdom from '../eventdom'
+import TodoList from '../todolist'
+import Storage from '../storage'
+
+const baseRoomTitle = '磊'
+
+export default [
+  {
+    path: '/demo',
+    name: 'DemoHome',
+    component: DemoHome,
+    meta: { title: baseRoomTitle },
+    children: [
+      {
+        path: '/demo-home',
+        name: 'demoHome',
+        component: DemoHome,
+        children: [
+          {
+            path: 'aaa',
+            name: 'aaa',
+            component: Aaa
+          }
+        ]
+      },
+      {
+        path: '/style',
+        name: 'style',
+        component: Style
+      },
+      {
+        path: '/eventdom',
+        name: 'eventdom',
+        component: Eventdom
+      },
+      {
+        path: '/todolist',
+        name: 'todolist',
+        component: TodoList
+      },
+      {
+        path: '/storage',
+        name: 'storage',
+        component: Storage
+      },
+      {
+        path: '/resource',
+        name: 'resource',
+        component: Resource
+      },
+      {
+        path: '/axios',
+        name: 'axios',
+        component: Axios
+      },
+      {
+        path: '/route',
+        name: 'route',
+        component: Route
+      },
+      {
+        path: '/routeDtail/:sb',
+        name: 'routeDtail',
+        component: RouteDtail
+      },
+      {
+        path: '/routeDtail2',
+        name: 'routeDtail2',
+        component: RouteDtail2
+      },
+      {
+        path: '/routeRes',
+        name: 'routeRes',
+        component: RouteRes
+      },
+      {
+        path: '/routePush',
+        name: 'routePush',
+        component: RoutePush
+      },
+      {
+        path: '/routeSon',
+        name: 'routeSon',
+        component: RouteSon,
+        children:[
+          {
+            path: 'routeSon1',
+            name: 'routeSon1',
+            component: RouteSon1
+          },
+          {
+            path: 'routeSon2',
+            name: 'routeSon2',
+            component: RouteSon2
+          },
+        ]
+      },
+      {
+        path: '/mintUI',
+        name: 'mintUI',
+        component: MintUI
+      },
+      {
+        path: '/elementUI',
+        name: 'elementUI',
+        component: ElementUI
+      },
+      {
+        path: '/es6',
+        name: 'es6',
+        component: Es6
+      }
+    ]
+  }
+]
--- src/router/index.js
+++ src/router/index.js
@@ -60,160 +60,26 @@
 
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import DemoHome from '@/pages/room-demo/home'
-import Aaa from '@/pages/room-demo/aaa'
-import Style from '@/pages/room-demo/style'
-import Eventdom from '@/pages/room-demo/eventdom'
-import TodoList from '@/pages/room-demo/todolist'
-import Storage from '@/pages/room-demo/storage'
-import Comp from '@/components/comp'
-import Resource from '@/pages/room-demo/resource'
-import Axios from '@/pages/room-demo/axios'
-import Route from '@/pages/room-demo/route'
-import RouteDtail from '@/pages/room-demo/routeDtail'
-import RouteDtail2 from '@/pages/room-demo/routeDtail2'
-import RouteRes from '@/pages/room-demo/routeRes'
-import RoutePush from '@/pages/room-demo/routePush'
-import RouteSon from '@/pages/room-demo/routeSon'
-import RouteSon1 from '@/pages/room-demo/routeSon1'
-import RouteSon2 from '@/pages/room-demo/routeSon2'
-import MintUI from '@/pages/room-demo/mintUI'
-import ElementUI from '@/pages/room-demo/elementUI'
-import Es6 from '@/pages/room-demo/es6'
+import DemoRouter from '@/pages/room-demo/router' // demo空间
+import LeigeRouter from '@/pages/room-leige/router' // 磊哥空间
 
-import LeigeRouter from '@/pages/leigeRoom/router' // 磊哥空间
-
-import Home from '@/pages/home'
-
-
+import Home from '@/pages/home' // 主页
 
 Vue.use(Router)
 
 const router = new Router({
-  //默认为hash模式 #   
-  //mode: 'history',  改为history模式  /形式展现   但是会有问题 比如index.html什么的 需要跟后端做配置，所以就用默认hash模式就好
   routes: [
-    ...LeigeRouter,
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/demo-home',
-      name: 'demoHome',
-      component: DemoHome,
-      children: [
-        {
-          path: 'aaa',
-          name: 'aaa',
-          component: Aaa
-        }
-      ]
-    },
-    {
-      path: '/style',
-      name: 'style',
-      component: Style
-    },
-    {
-      path: '/eventdom',
-      name: 'eventdom',
-      component: Eventdom
-    },
-    {
-      path: '/todolist',
-      name: 'todolist',
-      component: TodoList
-    },
-    {
-      path: '/storage',
-      name: 'storage',
-      component: Storage
-    },
-    {
-      path: '/comp',
-      name: 'comp',
-      component: Comp
-    },
-    {
-      path: '/resource',
-      name: 'resource',
-      component: Resource
-    },
-    {
-      path: '/axios',
-      name: 'axios',
-      component: Axios
-    },
-    {
-      path: '/route',
-      name: 'route',
-      component: Route
-    },
-    {
-      path: '/routeDtail/:sb',
-      name: 'routeDtail',
-      component: RouteDtail
-    },
-    {
-      path: '/routeDtail2',
-      name: 'routeDtail2',
-      component: RouteDtail2
-    },
-    {
-      path: '/routeRes',
-      name: 'routeRes',
-      component: RouteRes
-    },
-    {
-      path: '/routePush',
-      name: 'routePush',
-      component: RoutePush
-    },
-    {
-      path: '/routeSon',
-      name: 'routeSon',
-      component: RouteSon,
-      children:[
-        {
-          path: 'routeSon1',
-          name: 'routeSon1',
-          component: RouteSon1
-        },
-        {
-          path: 'routeSon2',
-          name: 'routeSon2',
-          component: RouteSon2
-        },
-      ]
-    },
-    {
-      path: '/mintUI',
-      name: 'mintUI',
-      component: MintUI
-    },
-    {
-      path: '/elementUI',
-      name: 'elementUI',
-      component: ElementUI
-    },
-    {
-      path: '/es6',
-      name: 'es6',
-      component: Es6
-    },
-    {
-      path: '/home',
-      name: 'home',
+      name: 'Home',
       component: Home
     },
-    //路由重定向--遇到没有设定的路由跳转到指定路由
-    {
+    { //路由重定向--遇到没有设定的路由跳转到指定路由
       path: '*',
       redirect: '/'
     },
+    ...DemoRouter,
+    ...LeigeRouter
   ]
 })
 

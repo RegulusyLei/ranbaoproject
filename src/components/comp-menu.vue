6af85c7db742450e08e4e931eec017a1a6698dbc
张磊 <zhanglei@everjiankang.com>
2019-06-30 23:04:53 +0800

    大宝的坑

--- src/components/comp-menu.vue
+++ src/components/comp-menu.vue
@@ -1,25 +1,6 @@
 <template>
   <div>
     <el-col :span="12">
-<<<<<<< HEAD
-      <!-- <h5>默认颜色</h5> -->
-      <el-menu :default-active="activeMenu" class="el-menu-vertical-demo" @select="handleSelect">
-        <el-submenu v-for="(item, index) in menu" :key="index" :index="item.index">
-          <template slot="title">
-            <i :class="item.icon"></i>
-            <span>{{item.name}}</span>
-          </template>
-          <!-- <el-menu-item-group> -->
-          <el-menu-item
-            v-for="(items, indexs) in item.list"
-            :key="indexs"
-            :index="items.index"
-          >{{items.name}}</el-menu-item>
-          <!-- </el-menu-item-group> -->
-        </el-submenu>
-      </el-menu>
-    </el-col>
-=======
     <!-- <h5>默认颜色</h5> -->
     <el-menu
       :default-active="activeMenu"
@@ -40,7 +21,6 @@
       </el-submenu>
     </el-menu>
   </el-col>
->>>>>>> 85822521ccd6e85a2137f0a46aad72851327ee22
   </div>
 </template>
 
@@ -53,17 +33,12 @@ export default {
     }
   },
   methods: {
-<<<<<<< HEAD
     handleSelect (index) {
       // console.log("index")
       // console.log(index)
       // console.log("indexPath")
       // console.log(indexPath)
       this.$router.push({ path: index })
-=======
-    handleSelect(index){
-      this.$router.push({path: index})
->>>>>>> 85822521ccd6e85a2137f0a46aad72851327ee22
     }
   },
   mounted () {},
@@ -76,13 +51,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-<<<<<<< HEAD
-.el-col {
-  width: 100%;
-}
-=======
   .el-col {
     width: 100%;
   }
->>>>>>> 85822521ccd6e85a2137f0a46aad72851327ee22
 </style>
--- src/pages/room-rose/pages/echarts.vue
+++ src/pages/room-rose/pages/echarts.vue
@@ -3,83 +3,88 @@
     <h2>echarts来啦</h2>
     <div class="echarts">
       <!-- 三角形 -->
-        <div class="arrow"></div>
-        <!-- 呼吸灯 -->
-        <div class="huxideng">呼吸灯</div>
-         <div id="container1" style="height: 100%"></div>
+      <div class="arrow"></div>
+      <!-- 呼吸灯 -->
+      <div class="huxideng">呼吸灯</div>
+      <div id="container1" style="height: 100%"></div>
     </div>
   </div>
 </template>
+
 <script>
 import echarts from 'echarts'
 export default {
-  data(){
-    return{
-
-    }
+  data () {
+    return {}
   },
-  mounted(){
-    var dom = document.getElementById("container1");
-			var myChart = echarts.init(dom);
-			var app = {};
-		var 	option = null;
-			option = {
-			    xAxis: {
-			        type: 'category',
-			        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-			    },
-			    yAxis: {
-			        type: 'value'
-			    },
-			    series: [{
-			        data: [820, 932, 901, 934, 1290, 1330, 1320],
-			        type: 'line'
-			    }]
-			};
-			;
-			if (option && typeof option === "object") {
-			    myChart.setOption(option, true);
-			}
+  mounted () {
+    var dom = document.getElementById('container1')
+    var myChart = echarts.init(dom)
+    // var app = {}
+    var option = null
+    option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line'
+        }
+      ]
+    }
+    if (option && typeof option === 'object') {
+      myChart.setOption(option, true)
+    }
   }
 }
 </script>
+
 <style lang="scss" scoped>
-
-  .echarts {
-    width: 80%;
-    margin: 0 auto;
-    height: 400px;
-    padding: 50px;
-    background-color: pink;
-    .arrow {
-      width: 0px;
-      height:0px;
-      border: 50px solid goldenrod;
-      // border-left: none;
-      border-top: none;
-      border-left: 50px solid transparent;
-      border-right: 50px solid transparent;
-    }
-    .huxideng {
-      width: 200px;
-      height: 80px;
-      border-radius: 20px;
-      background-color: brown;
-      text-align: center;
-      line-height: 80px;
-      color: #FFF;
-      font-size: 20px;
-      margin-top: 20px;
-      // box-shadow: 0 0 10px rgba($color: #F00, $alpha: 0.5);
-
-      // box-shadow: 0px 0px 20spx rgba($color: #F00, $alpha: 1);
-      animation: huxideng 1s infinite;
-    }
+.echarts {
+  width: 80%;
+  margin: 0 auto;
+  height: 400px;
+  padding: 50px;
+  background-color: pink;
+  .arrow {
+    width: 0px;
+    height: 0px;
+    border: 50px solid goldenrod;
+    // border-left: none;
+    border-top: none;
+    border-left: 50px solid transparent;
+    border-right: 50px solid transparent;
   }
-  @keyframes huxideng {
-   0% { box-shadow: 0px 0px 20px rgba($color: #000, $alpha: 1); }
-   50% {box-shadow: 0px 0px 20px rgba($color: #000, $alpha: 0.5); }
-   100% {box-shadow: 0px 0px 20px rgba($color: #000, $alpha: 1); }
+  .huxideng {
+    width: 200px;
+    height: 80px;
+    border-radius: 20px;
+    background-color: brown;
+    text-align: center;
+    line-height: 80px;
+    color: #fff;
+    font-size: 20px;
+    margin-top: 20px;
+    // box-shadow: 0 0 10px rgba($color: #F00, $alpha: 0.5);
+
+    // box-shadow: 0px 0px 20spx rgba($color: #F00, $alpha: 1);
+    animation: huxideng 1s infinite;
+  }
+}
+@keyframes huxideng {
+  0% {
+    box-shadow: 0px 0px 20px rgba($color: #000, $alpha: 1);
+  }
+  50% {
+    box-shadow: 0px 0px 20px rgba($color: #000, $alpha: 0.5);
+  }
+  100% {
+    box-shadow: 0px 0px 20px rgba($color: #000, $alpha: 1);
+  }
 }
 </style>
-

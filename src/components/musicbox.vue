2515a6a3fa7f2900154e6fd3c49cbfac35c7116b
张磊 <zhanglei@everjiankang.com>
2019-07-03 14:45:35 +0800

    测试音乐盒音量

--- src/components/musicbox.vue
+++ src/components/musicbox.vue
@@ -4,6 +4,8 @@
       <button>上一首</button>
       <button @click="playOrPaused">{{play ? '暂停' : '播放'}}</button>
       <button>下一首</button>
+      <span>当前音量：{{currentVolume}}</span>
+      <button @click="testFn">一键测试</button>
     </div>
     <audio ref="musicDom" :src="musicSrc" controls></audio>
   </div>
@@ -19,12 +21,21 @@ export default {
       play: false // 是否播放
     }
   },
+  computed: {
+    // 当前音量
+    currentVolume () {
+      return (this.musicDom && this.musicDom.volume) || 0
+    }
+  },
   mounted () {
     this.$nextTick(() => {
       this.musicDom = this.$refs.musicDom
     })
   },
   methods: {
+    testFn () {
+      this.musicDom.volume = 0.1
+    },
     playOrPaused () {
       if (this.play) {
         this.musicDom.pause()

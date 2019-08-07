3459de3863cda8899b9d5167503eec65a1ea79a1
张磊 <zhanglei@everjiankang.com>
2019-08-07 11:48:38 +0800

    input

--- src/pages/room-test/pages/test1.vue
+++ src/pages/room-test/pages/test1.vue
@@ -2,6 +2,10 @@
   <div>
     <com-a></com-a>
     <com-b></com-b>
+    <div class="test">
+      <div>箭头</div>
+      <input type="file">
+    </div>
   </div>
 </template>
 
@@ -15,3 +19,36 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .test {
+    width: 100px;
+    height: 40px;
+    position: relative;
+    overflow: hidden;
+    div {
+      height: 100%;
+      background-color: #409EFF;
+      color: #fff;
+      border-radius: 3px;
+      text-align: center;
+      line-height: 40px;
+    }
+    input {
+      position: absolute;
+      z-index: 1;
+      width: 0;
+      height: 0;
+      padding: 100px;
+      top: 0;
+      left: 0;
+      opacity: 0;
+      cursor: pointer;
+    }
+  }
+  .test:hover {
+    div {
+      color: pink;
+    }
+  }
+</style>
